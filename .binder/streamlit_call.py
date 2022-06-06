fd7ca700fb3f775e94e9d3a899dc71d5f076458c
Chris Turnadge <chris.turnadge@csiro.au>
2022-06-07 08:50:50 +0930

    Update streamlit_call.py

--- .binder/streamlit_call.py
+++ .binder/streamlit_call.py
@@ -2,4 +2,5 @@ from subprocess import Popen
 
 def load_jupyter_server_extension(nbapp):
     """serve the streamlit app"""
-    Popen(["streamlit", "hello", "--browser.serverAddress=0.0.0.0", "--server.enableCORS=False"])
+    #Popen(["streamlit", "hello", "--browser.serverAddress=0.0.0.0", "--server.enableCORS=False"])
+    Popen(["streamlit", "run", "st_test.py", "--browser.serverAddress=0.0.0.0", "--server.enableCORS=False"])
